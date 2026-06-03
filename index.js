af0b98fca3f6d45e80b93c4e5c6d9e977efa9b87
anish3441k <anishahmed35767@gmail.com>
2026-06-03 22:28:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -284,37 +284,37 @@ loadTrendingMovies();
 
 function openMovie(){
 
-window.location.href = "movies-browse.html";
+window.location.href='movies-browse.html';
 
 }
 
 function openWebSeries(){
 
-window.location.href = "webseries-browse.html";
+window.location.href='webseries-browse.html';
 
 }
 
 function openAnime(){
 
-window.location.href = "anime-browse.html";
+window.location.href='anime-browse.html';
 
 }
 
 function openAnimeMovie(){
 
-window.location.href = "anime-movies-browse.html";
+window.location.href='anime-movies-browse.html';
 
 }
 
 function openGame(){
 
-window.location.href = "games-browse.html";
+window.location.href='games-browse.html';
 
 }
 
 function openCartoon(){
 
-window.location.href = "cartoons-browse.html";
+window.location.href='cartoons-browse.html';
 
 }
 
